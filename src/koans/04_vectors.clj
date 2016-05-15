a378f2f4dd56b96884191ce946f5dd8b4869e3de
lemastero <adres.mistrza@gmail.com>
2016-05-15 03:47:31 +0200

    Solve koans 04 additional vector stuff

--- src/koans/04_vectors.clj
+++ src/koans/04_vectors.clj
@@ -6,28 +6,39 @@
   (= 1 (count [42]))
 
   "You can create a vector from a list"
-  (= [1] (vec '(1)))
+  (=
+    (= [1] (vec  '(1) ))
+    (= []  (vec  '()  ))
+    (= []  (vec  nil  )) )
 
   "Or from some elements"
-  (= [nil nil] (vector nil nil))
+  (= [nil nil]
+     (vector nil nil))
 
   "But you can populate it with any number of elements at once"
-  (= [1 2] (vec '(1 2)))
+  (= [1 2]
+     (vec '(1 2)) )
 
   "Conjoining to a vector is different than to a list"
-  (= [111 222 333] (conj [111 222] 333))
+  (= [111 222 333]
+     (conj [111 222] 333) )
 
   "You can get the first element of a vector like so"
-  (= :peanut (first [:peanut :butter :and :jelly]))
+  (= :peanut
+     (first [:peanut :butter :and :jelly]) )
 
   "And the last in a similar fashion"
-  (= :jelly (last [:peanut :butter :and :jelly]))
+  (= :jelly
+     (last [:peanut :butter :and :jelly]))
 
   "Or any index if you wish"
-  (= :jelly (nth [:peanut :butter :and :jelly] 3))
+  (= :jelly
+     (nth [:peanut :butter :and :jelly] 3))
 
   "You can also slice a vector"
-  (= [:butter :and] (subvec [:peanut :butter :and :jelly] 1 3))
+  (= [:butter :and]
+     (subvec [:peanut :butter :and :jelly] 1 3))
 
   "Equality with collections is in terms of values"
-  (= (list 1 2 3) (vector 1 2 3)))
+  (= (list 1 2 3)
+     (vector 1 2 3)))
